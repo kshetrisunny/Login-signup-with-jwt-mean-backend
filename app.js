c75a4513be56a68ee81bd6f38cbcafadbd8af290
kshetrisunny <kshetrisunny@outlook.com>
2020-03-28 19:49:18 +0530

    login signup with jwt(token) mean stack backend

---
+++ app.js
@@ -0,0 +1,56 @@
+require('./config/config');
+require('./models/db');
+require('./config/passportConfig');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+var app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/api', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
